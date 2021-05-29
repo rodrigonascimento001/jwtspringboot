81f1c0a1a84bbb3eb6a6fdd06b301667a4f51b66
root <rodrigo_allexandre@yahoo.com.br>
2021-05-29 18:26:36 -0300

    initial commit

---
+++ src/main/java/com/example/jwt/authprojectjwt/AuthprojectjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt.authprojectjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthprojectjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthprojectjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt/authprojectjwt/AuthprojectjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt.authprojectjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthprojectjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
